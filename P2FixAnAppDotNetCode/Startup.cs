fe02e4c0d1672abe520bcf4237855a1ea15e139c
KingOfZeHill <kingofzehill@gmail.com>
2023-12-12 17:30:05 +0100

    Fix for BUG 02 : spanish cultres support : - add 9 ressources (.resx) files for spanish translations. - update Startup.cs for adding spanish cultures managment.

--- P2FixAnAppDotNetCode/Startup.cs
+++ P2FixAnAppDotNetCode/Startup.cs
@@ -49,6 +49,9 @@ namespace P2FixAnAppDotNetCode
                     new CultureInfo("en"),
                     new CultureInfo("fr-FR"),
                     new CultureInfo("fr"),
+                    // SMO: BUG B02 add spanich cultures to supportedCultures
+                    new CultureInfo("es-ES"),
+                    new CultureInfo("es"),
                 };
 
                 opts.DefaultRequestCulture = new RequestCulture("en");
